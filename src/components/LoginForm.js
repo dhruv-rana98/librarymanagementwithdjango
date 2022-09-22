0e609d2facfbfc37cb0ba4a9e091211626e52dfc
Dhruv Rana <dhruvrana0000@gmail.com>
2022-09-22 11:16:00 +0530

    Login Form changes to redirect

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,22 +1,45 @@
 import React, { useState, memo, useEffect } from "react";
 import "./style.css";
+import { useHistory } from "react-router-dom";
 const LoginForm = (props) => {
+  const history = useHistory();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [submit, setSubmit] = useState(false);
+  const [users, setUsers] = useState([]);
+  const [response, setResponse] = useState(false);
+  const [error, setError] = useState(null);
   const emailChangeHandler = (e) => {
     setEmail(e.target.value);
   };
   const passwordChangeHandler = (e) => {
     setPassword(e.target.value);
   };
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/users")
+      .then((response) => response.json())
+      .then((data) => {
+        setUsers(data);
+      })
+      .catch((err) => setError(err));
+  }, []);
   const onSubmitHandler = (e) => {
     e.preventDefault();
     console.log(email, password);
-    fetch("http://127.0.0.1:8000/users")
-      .then((response) => response.json())
-      .then((data) => console.log(data))
-      .catch((err) => console.log(err));
+    console.log(users);
+
+    const fetchedList = users.filter((user) => {
+      return user.AdminEmail === email && user.AdminPassword === password;
+    });
+    if (fetchedList.length === 1 && fetchedList.isAdmin === 1) {
+      history.push("/admin");
+    } else if (fetchedList.length === 1 && fetchedList.isAdmin === 0) {
+      history.push("/student");
+    } else {
+      setResponse(false);
+      setError("Duplicate records found, please contact with admin to update");
+    }
     setEmail("");
     setPassword("");
   };
@@ -57,6 +80,10 @@ const LoginForm = (props) => {
           <button disabled={!submit}>Submit</button>
         </div>
       </form>
+      <div className="output">
+        {response && <p>Sending to the library page, please wait.</p>}
+        {error && <p>error</p>}
+      </div>
     </div>
   );
 };
